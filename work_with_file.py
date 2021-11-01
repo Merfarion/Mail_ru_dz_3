39898c50ec0a8ac4c7b6f4fa763cf1202fe26450
Merfarion <64864827+Merfarion@users.noreply.github.com>
2021-11-01 08:26:19 +0300

    все три пункта работают

--- work_with_file.py
+++ work_with_file.py
@@ -1,26 +1,22 @@
-# import os
-# script_dir = os.path.dirname(__file__)
-# path = "Test_Mail\log.log"
-# abs_file_path = os.path.join(script_dir, path)
-# print(abs_file_path)
-# f = open(abs_file_path, "r")
-# print(f.read())
-from pathlib import Path
 import os
 import re
+
 script_dir = os.path.dirname(__file__)
 path = input()
 pattern = re.compile(' [a-zA-Z]{7}: [a-zA-Z]{7}:')
 abs_file_path = os.path.join(script_dir, path)
-print(abs_file_path)
+result = open('result.log', 'w')
+# print(abs_file_path)
 if abs_file_path.find('.log') != -1:
     f = open(abs_file_path, "r")
-    # print(f.read())
+    for line in f:
+        print(line)
+        if pattern.findall(line):
+            result.write(line + '\n')
 else:
     for file in os.listdir(abs_file_path):
-        print(file)
-        with open(os.path.join(abs_file_path, file),'r') as f:
+        with open(os.path.join(abs_file_path, file), 'r') as f:
             for line in f:
-                 if pattern.findall(line):
-                    print(line)
-            # print(f.read())
+                print(line)
+                if pattern.findall(line):
+                    result.write(line+'\n')
