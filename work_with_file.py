d3a8115e5a09efb205013a9488b9cc2f4157a4ae
Merfarion <64864827+Merfarion@users.noreply.github.com>
2021-11-01 08:00:12 +0300

    2 пункт задания робит(наверное)

--- work_with_file.py
+++ work_with_file.py
@@ -10,13 +10,17 @@ import os
 import re
 script_dir = os.path.dirname(__file__)
 path = input()
+pattern = re.compile(' [a-zA-Z]{7}: [a-zA-Z]{7}:')
 abs_file_path = os.path.join(script_dir, path)
 print(abs_file_path)
-if (abs_file_path.find('.log')!=-1):
+if abs_file_path.find('.log') != -1:
     f = open(abs_file_path, "r")
-    print(f.read())
+    # print(f.read())
 else:
     for file in os.listdir(abs_file_path):
         print(file)
         with open(os.path.join(abs_file_path, file),'r') as f:
-            print(f.read())
+            for line in f:
+                 if pattern.findall(line):
+                    print(line)
+            # print(f.read())
