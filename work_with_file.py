dd6ad979545b8fa20f1b1259d872d0d6c6bfe066
Merfarion <64864827+Merfarion@users.noreply.github.com>
2021-11-01 13:30:51 +0300

    Исправление мелкого косяка

--- work_with_file.py
+++ work_with_file.py
@@ -8,15 +8,16 @@ abs_file_path = os.path.join(script_dir, path)  # Прикрепление к п
 result = open('result.log', 'w')  # Открытие файла на запись для результатов поиска
 # print(abs_file_path)  # Вывод пути
 if abs_file_path.find('.log') != -1:  # Проверка на то, что у нас путь к файлу
-    f = open(abs_file_path, "r")  # Открытие файла на чтение
-    for line in f:  # Цикл для взаимодействия с файлом построчно
-        print(line)  # Вывод строки
-        if pattern.findall(line):  # Проверяем, есть ли совпадение с шаблоном
-            result.write(line + '\n')  # Запись подходящих строк в файл
+    with open(abs_file_path, "r") as f:  # Открытие файла на чтение
+        for line in f:  # Цикл для взаимодействия с файлом построчно
+            print(line)  # Вывод строки
+            if pattern.findall(line):  # Проверяем, есть ли совпадение с шаблоном
+                result.write(line + '\n')  # Запись подходящих строк в файл
 else:  # Иначе)
     for file in os.listdir(abs_file_path):  # Цикл для перечисление файлов в директории
         with open(os.path.join(abs_file_path, file), 'r') as f:  # Открытие файла на чтение
             for line in f:  # Цикл для взаимодействия с файлом построчно
                 print(line)  # Вывод строки
-                if pattern.findall(line):   # Проверяем, есть ли совпадение с шаблоном
-                    result.write(line + '\n')   # Запись подходящих строк в файл
+                if pattern.findall(line):  # Проверяем, есть ли совпадение с шаблоном
+                    result.write(line + '\n')  # Запись подходящих строк в файл
+result.close()  # Закрытие файла
